3bdba8cfe16c7ea8efad267158c8a953f5721dc2
arpita-devcode <arpitadebnath940@gmail.com>
2026-03-09 21:13:27 +0600

    Marathon

--- Problem-70Marathon.cpp
+++ Problem-70Marathon.cpp
@@ -1,1 +1,31 @@
-https://codeforces.com/contest/1692/problem/A
+// https://codeforces.com/contest/1692/problem/A
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a, b, c, d;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+        cin >> a >> b >> c >> d;
+        if (a < b)
+        {
+            count++;
+        }
+        if (a < c)
+        {
+            count++;
+        }
+        if (a < d)
+        {
+            count++;
+        }
+        cout << count <<endl;
+    }
+
+    return 0;
+}
