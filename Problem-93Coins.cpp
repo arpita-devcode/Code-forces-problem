47cf1f82d2f5b41749c0983c5c8ff3b6905adfe9
arpita-devcode <arpitadebnath940@gmail.com>
2026-03-10 21:56:53 +0600

    Coins

---
+++ Problem-93Coins.cpp
@@ -0,0 +1,23 @@
+//https://codeforces.com/contest/1814/problem/A
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        long long n,k;
+        cin >> n >> k;
+
+        if(n % 2 == 0)
+            cout << "YES" << endl;
+        else if(k % 2 == 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
