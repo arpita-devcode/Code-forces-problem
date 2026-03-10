627553b4f6380c254ea40611328ac1ef1c28afed
arpita-devcode <arpitadebnath940@gmail.com>
2026-03-10 20:28:38 +0600

    Odd Set

---
+++ Problem-87Odd-Set.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        int odd = 0, even = 0;
+
+        for(int i = 0; i < 2*n; i++){
+            int x;
+            cin >> x;
+
+            if(x % 2 == 0) even++;
+            else odd++;
+        }
+
+        if(odd == even) cout << "Yes\n";
+        else cout << "No\n";
+    }
+
+    return 0;
+}
