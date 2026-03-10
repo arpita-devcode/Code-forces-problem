45efe6593522fdeeb05a9fc7212468c2faa40c6d
arpita-devcode <arpitadebnath940@gmail.com>
2026-03-10 22:36:24 +0600

    Magical Sticks

--- Problem-98Magical-Sticks.cpp
+++ Problem-98Magical-Sticks.cpp
@@ -1,6 +1,6 @@
 //https://codeforces.com/contest/1371/problem/A
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -9,8 +9,7 @@ int main() {
     while(t--) {
         long long n;
         cin >> n;
-        long long sum = n * (n + 1) / 2; // sum of 1..n
-        cout << sum << endl;
+        cout << (n + 1) / 2 << "\n"; // ceil(n/2)
     }
     return 0;
 }
