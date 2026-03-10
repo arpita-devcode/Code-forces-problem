a5e5175726db3f2e6b47aeae2af3ba7ff1b7ad96
arpita-devcode <arpitadebnath940@gmail.com>
2026-03-10 13:44:12 +0600

    Easy Problem

---
+++ Problem-83Easy-Problem.cpp
@@ -0,0 +1,17 @@
+//https://codeforces.com/contest/2044/problem/A
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n; i++){
+        int x;
+        cin>>x;
+        int res = x-1;
+        cout<<res<<endl;
+    }
+
+    return 0;
+}
